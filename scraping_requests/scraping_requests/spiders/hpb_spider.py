036b5ac87fd475b876b543c352cd2921179fb5c4
abegon-ta <abe.taku@icloud.com>
2024-02-02 14:45:52 +0900

    他の項目を取得できるように変更した

--- scraping_requests/scraping_requests/spiders/hpb_spider.py
+++ scraping_requests/scraping_requests/spiders/hpb_spider.py
@@ -1,4 +1,6 @@
 import re
+import requests
+from lxml import html
 
 import scrapy
 from scrapy.linkextractors import LinkExtractor
@@ -25,6 +27,18 @@ class HpbSpiderSpider(CrawlSpider):
 
         salon_url = response.url
 
+        address = response.xpath('//th[text()="住所"]/following-sibling::td/text()').get()
+        if address:
+            address = re.sub('\s', '', address)
+        
+        tel_url = response.xpath('//th[text()="電話番号"]/following-sibling::td/a/@href').get()
+        if tel_url:
+            res_for_tel = requests.get(tel_url)
+            res_for_tel = html.fromstring(res_for_tel.text)
+            tel = res_for_tel.xpath('//th[text()="電話番号  "]/following-sibling::td/text()')
+            if tel:
+                tel = re.sub('\s', '', tel[0])
+
         number_of_seats = response.xpath('//th[text()="席数"]/following-sibling::td/text()').get()
         if number_of_seats:
             number_of_seats = re.sub('\s', '', number_of_seats)
@@ -32,10 +46,13 @@ class HpbSpiderSpider(CrawlSpider):
         stylists = response.xpath('//th[text()="スタッフ数"]/following-sibling::td[1]/text()').get()
         if stylists:
             stylists = re.sub('\s', '', stylists)
+        
 
         yield {
             "salon_name" : salon_name,
             "salon_url" : salon_url,
+            "address" : address,
+            "tel" : tel,
             "number_of_seats" : number_of_seats,
             "stylists" : stylists
         }
